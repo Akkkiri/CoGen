47d28574ee97c96f00d42483ee79a4fe3edc0210
serin-B <bangserin980302@gmail.com>
2023-04-04 01:38:45 +0900

    fix: Nav에 isLogin state 추가

--- fe/src/components/Layout/Nav.tsx
+++ fe/src/components/Layout/Nav.tsx
@@ -2,10 +2,12 @@ import { IoExtensionPuzzleOutline } from "react-icons/io5";
 import { BsPatchQuestion, BsPerson } from "react-icons/bs";
 import { SlSpeech, SlHome } from "react-icons/sl";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useAppSelector } from "store/hook";
+import { isLogin } from "store/modules/authSlice";
 
 export default function Nav() {
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const isLoginUser = useAppSelector(isLogin);
+
   return (
     <nav className="w-full m-auto fixed bottom-0 z-[9] border-t border-y-lightGray bg-white">
       <div className="flex justify-between max-w-5xl m-auto  pt-2">
@@ -54,7 +56,7 @@ export default function Nav() {
         </div>
         <div className="w-20">
           <NavLink
-            to={isLogin ? "/mypage" : "/login"}
+            to={isLoginUser ? "/mypage" : "/login"}
             className={({ isActive }) => (isActive ? "text-y-red" : "")}
           >
             <div className="flex flex-col items-center text-sm">
