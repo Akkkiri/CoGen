e8ac9839af559a60f321f366b0a02df40007c359
gnidinger <gcnb87@gmail.com>
2023-04-07 23:49:23 +0900

    CORS

--- be/src/main/java/ewha/backend/global/security/CorsConfig.java
+++ be/src/main/java/ewha/backend/global/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			// .allowedOrigins("*")
-			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("http://localhost:3000", "https://www.akkkiri.co.kr/")
 			.allowedMethods("*")
 			.exposedHeaders("Authorization")
 			.allowCredentials(true)
