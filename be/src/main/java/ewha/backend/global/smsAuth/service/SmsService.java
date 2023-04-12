518525e07622c6e5a003c875ffb19142c11d5644
gnidinger <gcnb87@gmail.com>
2023-04-13 03:28:01 +0900

    Minor Bug Fixed

--- be/src/main/java/ewha/backend/global/smsAuth/service/SmsService.java
+++ be/src/main/java/ewha/backend/global/smsAuth/service/SmsService.java
@@ -6,12 +6,14 @@ import java.util.HashMap;
 import java.util.Random;
 
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
+import ewha.backend.domain.user.entity.User;
 import ewha.backend.domain.user.repository.UserRepository;
 import ewha.backend.domain.user.service.UserService;
 import ewha.backend.global.smsAuth.dto.SmsDto;
@@ -24,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SmsService {
 
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	private final UserService userService;
 	private final UserRepository userRepository;
 	private final SmsRedisRepository smsRedisRepository;
@@ -57,23 +60,25 @@ public class SmsService {
 		Message coolsms = new Message(API_KEY, API_SECRET);
 
 		Random rand = new Random();
-		StringBuilder certificationNumber = new StringBuilder();
+		StringBuilder tempPassword = new StringBuilder();
 		for (int i = 0; i < 8; i++) {
 			String ran = Integer.toString(rand.nextInt(10));
-			certificationNumber.append(ran);
+			tempPassword.append(ran);
 		}
+
+		User findUser = userService.findByUserId(phoneNumber);
+		findUser.updatePassword(bCryptPasswordEncoder.encode(tempPassword));
+		userRepository.save(findUser);
 
 		HashMap<String, String> params = new HashMap<>();
 
 		params.put("to", phoneNumber);
 		params.put("from", "01030380831");
 		params.put("type", "SMS");
-		params.put("text", "우리 함께 CoGen: 임시 비밀번호는" + "[" + certificationNumber + "]" + "입니다.");
+		params.put("text", "우리 함께 CoGen: 임시 비밀번호는" + "[" + tempPassword + "]" + "입니다. 절대 외부로 유출하지 마세요.");
 		params.put("app_version", "test app 1.0");
 
 		coolsms.send(params);
-
-		smsRedisRepository.createCertification(phoneNumber, certificationNumber.toString());
 	}
 
 	public String verifyCertification(SmsDto.CertificationRequest request) {
