50c6521c8a0607ab7908d05f7232da050bfc56c0
SeongYum <dbal6930@gmail.com>
2023-04-26 18:04:58 +0900

    style:메인 로그인 유도 글씨 변경

--- fe/src/components/Main/MainUser.tsx
+++ fe/src/components/Main/MainUser.tsx
@@ -1,9 +1,9 @@
-import Level from "../user/Level";
-import { isLogin } from "../../store/modules/authSlice";
-import { useAppSelector } from "../../store/hook";
-import { NavLink } from "react-router-dom";
-import { Point } from "util/LevelUtil";
-import { medal, medalImg } from "components/user/UserProfile";
+import Level from '../user/Level';
+import { isLogin } from '../../store/modules/authSlice';
+import { useAppSelector } from '../../store/hook';
+import { NavLink } from 'react-router-dom';
+import { Point } from 'util/LevelUtil';
+import { medal, medalImg } from 'components/user/UserProfile';
 
 export interface UserProfileProps {
   nickname: string;
@@ -23,34 +23,15 @@ export default function MainUser({
   const isLoginUser = useAppSelector(isLogin);
   return (
     <div className="p-3 mx-2 my-4 rounded-xl border-2 border-y-lightGray  md:text-lg">
-      <div>
-        <img
-          src="/images/cogenlogo-p.png"
-          alt="logo"
-          className="w-9 float-right"
-        ></img>
-      </div>
       {isLoginUser ? (
         <NavLink to={`/mypage`}>
           <div className="flex justify-center items-center mx-2">
-            <img
-              src={profileImage}
-              alt="profileImage"
-              className="rounded-full w-20 h-20"
-            ></img>
+            <img src={profileImage} alt="profileImage" className="rounded-full w-20 h-20"></img>
             <div className="w-full mx-4 mt-2">
               <div className="flex items-end">
-                <span className={`${nickname.length === 8 ? "text-sm" : ""}`}>
-                  {nickname}
-                </span>
-                <span className="text-xs text-y-lightGray font-light">
-                  {hashcode}
-                </span>
-                <img
-                  src={medalImg[medal(level)]}
-                  alt="level"
-                  className="w-5 h-5"
-                ></img>
+                <span className={`${nickname.length === 8 ? 'text-sm' : ''}`}>{nickname}</span>
+                <span className="text-xs text-y-lightGray font-light">{hashcode}</span>
+                <img src={medalImg[medal(level)]} alt="level" className="w-5 h-5"></img>
               </div>
               <Level level={level} ariFactor={ariFactor} />
             </div>
@@ -58,13 +39,12 @@ export default function MainUser({
         </NavLink>
       ) : (
         <div className="p-2 text-center">
-          <div className="pb-3">
-            로그인 후 코젠에서 다양한 활동을 즐겨보세요!!
+          <div className="pb-3 md:text-xl">
+            <p>로그인 후 코젠에서</p>
+            <p>다양한 활동을 즐겨보세요</p>
           </div>
           <NavLink to="/login">
-            <button className="btn-p rounded-lg px-4 py-2 text-white text-sm">
-              로그인
-            </button>
+            <button className="btn-p rounded-lg px-4 py-2 text-white text-sm">로그인</button>
           </NavLink>
         </div>
       )}
